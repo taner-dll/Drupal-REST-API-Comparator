64dcd4de6b9576890f83e9fee1ab63eba5efa6c1
Taner Deliloglu <tnrdll@gmail.com>
2022-05-25 14:24:08 +0300

    completed content comparison for bundle

--- src/Controller/ComparisonController.php
+++ src/Controller/ComparisonController.php
@@ -31,6 +31,121 @@ class ComparisonController extends AbstractController
         ]);
     }
 
+
+    /**
+     * @Route("/comparison/compare-contents",
+     *     name="app_comparison_compare_contents",
+     *     options = { "expose" = true })
+     */
+    public function compareContents(Request $request): JsonResponse
+    {
+
+
+        $sourceURL = $request->request->get('source_url');
+        $targetURL = $request->request->get('target_url');
+
+        //dump($targetURL, $sourceURL);exit;
+
+
+        $responseSource = $this->client->request(
+            'GET',
+            $sourceURL
+        );
+
+        $responseTarget = $this->client->request(
+            'GET',
+            $targetURL
+        );
+
+        $source = $responseSource->toArray();
+        $target = $responseTarget->toArray();
+
+
+        //todo parse url for content type.
+        //todo if content type == bundle?
+
+
+        ## BUNDLE
+
+
+        $sourceTitle = [];
+        $sourceSearchTerm = [];
+        foreach ($source as $item => $value) {
+            $sourceTitle[] = $value['title'][0]['value'];
+            $sourceSearchTerm[] = $value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value']
+                .'-'.$value['langcode'][0]['value'];
+        }
+
+        //dump($sourceSearchTerm);exit;
+
+        $contents = [];
+        $missing_total = 0;
+        $changed_total = 0;
+        foreach ($target as $item => $value) {
+            if (!in_array($value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'], $sourceSearchTerm)):
+
+                $target_data = 'Title: '.$value['title'][0]['value'].' - '.
+                    'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];
+
+                $contents[] = [
+                    'type' => 'Missing',
+                    'key' => $value['type'][0]['target_id'],
+                    'source_value' => '',
+                    'target_value' => $target_data
+                ];
+
+                $missing_total++;
+
+            else:
+                if (!in_array($value['title'][0]['value'], $sourceTitle)):
+
+                    $target_data = 'Title: '.$value['title'][0]['value'].' - '.
+                        'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];
+
+                    $contents[] = [
+                        'type' => 'Changed',
+                        'key' => $value['type'][0]['target_id'],
+                        'source_value' => '',
+                        'target_value' => $target_data
+                    ];
+
+                    $changed_total++;
+                endif;
+            endif;
+
+        }
+
+
+        $targetSearchTerm = [];
+        foreach ($target as $item => $value) {
+            $targetSearchTerm[] = $value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value']
+                .'-'.$value['langcode'][0]['value'];
+        }
+        foreach ($source as $item => $value) {
+
+            if (!in_array($value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'], $targetSearchTerm)):
+
+                $source_data = 'Title: '.$value['title'][0]['value'].' - '.
+                    'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];;
+
+                $contents[] = [
+                    'type' => 'Missing',
+                    'key' => $value['type'][0]['target_id'],
+                    'source_value' => $source_data,
+                    'target_value' => ''
+                ];
+            endif;
+
+        }
+
+
+        dump($missing_total);exit;
+
+        return new JsonResponse($contents, $responseSource->getStatusCode());
+
+
+    }
+
     /**
      * @Route("/comparison/compare-translations",
      *     name="app_comparison_compare_translations",
