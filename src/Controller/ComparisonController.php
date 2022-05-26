80cfd7c80ef19242a5bd6cfd089a6dcfbaabfde8
Taner Deliloglu <tnrdll@gmail.com>
2022-05-26 04:13:55 +0300

    completed content comparison

--- src/Controller/ComparisonController.php
+++ src/Controller/ComparisonController.php
@@ -13,212 +13,281 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class ComparisonController extends AbstractController
 {
 
-    private $client;
+  private $client;
 
 
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
-    }
+  public function __construct(HttpClientInterface $client)
+  {
+    $this->client = $client;
+  }
 
-    /**
-     * @Route("/comparison/translations", name="app_comparison_translations")
-     */
-    public function translations(): Response
-    {
-        return $this->render('comparison/translations.html.twig', [
-            'controller_name' => 'ComparisonController',
-        ]);
-    }
-
-
-    /**
-     * @Route("/comparison/compare-contents",
-     *     name="app_comparison_compare_contents",
-     *     options = { "expose" = true })
-     */
-    public function compareContents(Request $request): JsonResponse
-    {
+  /**
+   * @Route("/comparison/translations", name="app_comparison_translations")
+   */
+  public function translations(): Response
+  {
+    return $this->render('comparison/translations.html.twig', [
+      'controller_name' => 'ComparisonController',
+    ]);
+  }
 
 
-        $sourceURL = $request->request->get('source_url');
-        $targetURL = $request->request->get('target_url');
-
-        //dump($targetURL, $sourceURL);exit;
-
-
-        $responseSource = $this->client->request(
-            'GET',
-            $sourceURL
-        );
-
-        $responseTarget = $this->client->request(
-            'GET',
-            $targetURL
-        );
-
-        $source = $responseSource->toArray();
-        $target = $responseTarget->toArray();
+  /**
+   * @Route("/comparison/compare-contents",
+   *     name="app_comparison_compare_contents",
+   *     options = { "expose" = true })
+   */
+  public function compareContents(Request $request): JsonResponse
+  {
 
 
-        //todo parse url for content type.
-        //todo if content type == bundle?
+    $sourceURL = $request->request->get('source_url');
+    $targetURL = $request->request->get('target_url');
+
+    //dump($targetURL, $sourceURL);exit;
 
 
-        ## BUNDLE
+    $responseSource = $this->client->request(
+      'GET',
+      $sourceURL
+    );
+
+    $responseTarget = $this->client->request(
+      'GET',
+      $targetURL
+    );
+
+    $source = $responseSource->toArray();
+    $target = $responseTarget->toArray();
+
+    $type = explode("?type=",$sourceURL);
+    $type = explode("&",$type[1]);
+    $type = $type[0];
+
+    //dump($type);exit;
+
+    $contents = [];
+
+    if ($type === 'bundle'){
+
+      $sourceTitle = [];
+      $sourceSearchTerm = [];
+      foreach ($source as $value) {
+        $sourceTitle[] = $value['title'][0]['value'];
+        $sourceSearchTerm[] = $value['uuid'][0]['value'];
+      }
+
+      foreach ($target as $value) {
+
+        if (!in_array($value['uuid'][0]['value'], $sourceSearchTerm)):
+
+          $target_data = 'Title: ' . $value['title'][0]['value'] . ' - ' .
+            'Code: ' . $value['field_bundle_code'][0]['value'];
+
+          $contents[] = [
+            'type' => 'Missing',
+            'key' => $value['type'][0]['target_id'],
+            'source_value' => '',
+            'target_value' => $target_data
+          ];
+        else:
+          if (!in_array($value['title'][0]['value'], $sourceTitle)):
+
+            $target_data = 'Title: ' . $value['title'][0]['value'] . ' - ' .
+              'Code: ' . $value['field_bundle_code'][0]['value'];
+
+            //Aynı UUID'ye sahip source içeriğine ait title'ı getir.
+            $bundleSourceTitle='';
+            foreach ($source as $val) {
+              if ($val['uuid'][0]['value'] === $value['uuid'][0]['value']) {
+                $bundleSourceTitle = 'Title: ' . $val['title'][0]['value'] . ' - ' .
+                  'Code: ' . $value['field_bundle_code'][0]['value'];
+              }
+            }
+
+            $contents[] = [
+              'type' => 'Changed',
+              'key' => $value['type'][0]['target_id'].' ('.$value['uuid'][0]['value'].')',
+              'source_value' => $bundleSourceTitle,
+              'target_value' => $target_data
+            ];
+          endif;
+        endif;
+      }
 
 
-        $sourceTitle = [];
-        $sourceSearchTerm = [];
-        foreach ($source as $item => $value) {
-            $sourceTitle[] = $value['title'][0]['value'];
-            $sourceSearchTerm[] = $value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value']
-                .'-'.$value['langcode'][0]['value'];
-        }
+      $targetSearchTerm = [];
+      foreach ($target as $value) {
+        $targetSearchTerm[] = $value['uuid'][0]['value'];
+      }
+      foreach ($source as $value) {
 
-        //dump($sourceSearchTerm);exit;
+        if (!in_array($value['uuid'][0]['value'], $targetSearchTerm)):
 
-        $contents = [];
-        $missing_total = 0;
-        $changed_total = 0;
-        foreach ($target as $item => $value) {
-            if (!in_array($value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'], $sourceSearchTerm)):
+          $source_data = 'Title: ' . $value['title'][0]['value'] . ' - ' .
+            'Code: ' . $value['field_bundle_code'][0]['value'];
 
-                $target_data = 'Title: '.$value['title'][0]['value'].' - '.
-                    'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];
-
-                $contents[] = [
-                    'type' => 'Missing',
-                    'key' => $value['type'][0]['target_id'],
-                    'source_value' => '',
-                    'target_value' => $target_data
-                ];
-
-                $missing_total++;
-
-            else:
-                if (!in_array($value['title'][0]['value'], $sourceTitle)):
-
-                    $target_data = 'Title: '.$value['title'][0]['value'].' - '.
-                        'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];
-
-                    $contents[] = [
-                        'type' => 'Changed',
-                        'key' => $value['type'][0]['target_id'],
-                        'source_value' => '',
-                        'target_value' => $target_data
-                    ];
-
-                    $changed_total++;
-                endif;
-            endif;
-
-        }
+          $contents[] = [
+            'type' => 'Missing',
+            'key' => $value['type'][0]['target_id'],
+            'source_value' => $source_data,
+            'target_value' => ''
+          ];
+        endif;
+      }
 
 
-        $targetSearchTerm = [];
-        foreach ($target as $item => $value) {
-            $targetSearchTerm[] = $value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value']
-                .'-'.$value['langcode'][0]['value'];
-        }
-        foreach ($source as $item => $value) {
+    }else{
 
-            if (!in_array($value['uuid'][0]['value'].'-'.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'], $targetSearchTerm)):
+      $sourceTitle = [];
+      $sourceSearchTerm = [];
+      foreach ($source as $value) {
+        $sourceTitle[] = $value['title'][0]['value'];
+        $sourceSearchTerm[] = $value['uuid'][0]['value'];
+      }
 
-                $source_data = 'Title: '.$value['title'][0]['value'].' - '.
-                    'Code: '.$value['field_bundle_code'][0]['value'].'-'.$value['langcode'][0]['value'];;
+      foreach ($target as $value) {
 
-                $contents[] = [
-                    'type' => 'Missing',
-                    'key' => $value['type'][0]['target_id'],
-                    'source_value' => $source_data,
-                    'target_value' => ''
-                ];
-            endif;
+        if (!in_array($value['uuid'][0]['value'], $sourceSearchTerm)):
 
-        }
+          $target_data = 'Title: ' . $value['title'][0]['value'];
 
+          $contents[] = [
+            'type' => 'Missing',
+            'key' => $value['type'][0]['target_id'],
+            'source_value' => '',
+            'target_value' => $target_data
+          ];
+        else:
+          if (!in_array($value['title'][0]['value'], $sourceTitle)):
 
-        dump($missing_total);exit;
+            $target_data = 'Title: ' . $value['title'][0]['value'];
 
-        return new JsonResponse($contents, $responseSource->getStatusCode());
+            //Aynı UUID'ye sahip source içeriğine ait title'ı getir.
+            $sourceTitle='';
+            foreach ($source as $val) {
+              if ($val['uuid'][0]['value'] === $value['uuid'][0]['value']) {
+                $sourceTitle = $val['title'][0]['value'];
+              }
+            }
 
+            $contents[] = [
+              'type' => 'Changed',
+              'key' => $value['type'][0]['target_id'].' ('.$value['uuid'][0]['value'].')',
+              'source_value' => $sourceTitle,
+              'target_value' => $target_data
+            ];
 
-    }
+          endif;
 
-    /**
-     * @Route("/comparison/compare-translations",
-     *     name="app_comparison_compare_translations",
-     *     options = { "expose" = true })
-     */
-    public function compareTranslations(Request $request): JsonResponse
-    {
-
-
-        $sourceURL = $request->request->get('source_url');
-        $targetURL = $request->request->get('target_url');
-
-
-        $responseSource = $this->client->request(
-            'GET',
-            $sourceURL
-        );
-
-        $responseTarget = $this->client->request(
-            'GET',
-            $targetURL
-        );
-
-        $source = $responseSource->toArray();
-        $target = $responseTarget->toArray();
+          //TODO Güncellenme tarihleri farklı ise UPDATED bul. renk mavi olabilir.
+          //TODO content type yanında (uuid) belirt.
+        endif;
+      }
 
 
-        $translations = [];
-        foreach ($target as $item => $value) {
-            if (!array_key_exists($item, $source)):
-                $translations[] = [
-                    'type' => 'Missing',
-                    'key' => $item,
-                    'source_value' => '',
-                    'target_value' => $value
-                ];
-            else:
-                if ($source[$item] !== $value):
-                    $translations[] = [
-                        'type' => 'Changed',
-                        'key' => $item,
-                        'source_value' => $source[$item],
-                        'target_value' => $value
-                    ];
-                endif;
-            endif;
-        }
+      $targetSearchTerm = [];
+      foreach ($target as $value) {
+        $targetSearchTerm[] = $value['uuid'][0]['value'];
+      }
+      foreach ($source as $value) {
 
-        foreach ($source as $item => $value) {
+        if (!in_array($value['uuid'][0]['value'], $targetSearchTerm)):
 
-            if (!array_key_exists($item, $target)):
-                $translations[] = [
-                    'type' => 'Missing',
-                    'key' => $item,
-                    'source_value' => $value,
-                    'target_value' => ''
-                ];
-            endif;
+          $source_data = 'Title: ' . $value['title'][0]['value'] . '-' . $value['langcode'][0]['value'];
 
-        }
-
-        return new JsonResponse($translations, $responseSource->getStatusCode());
-
+          $contents[] = [
+            'type' => 'Missing',
+            'key' => $value['type'][0]['target_id'],
+            'source_value' => $source_data,
+            'target_value' => ''
+          ];
+        endif;
+      }
 
     }
 
-    /**
-     * @Route("/comparison/contents", name="app_comparison_contents")
-     */
-    public function contents(): Response
-    {
-        return $this->render('comparison/contents.html.twig', [
-            'controller_name' => 'ComparisonController',
-        ]);
+
+
+
+    return new JsonResponse($contents, $responseSource->getStatusCode());
+
+
+  }
+
+  /**
+   * @Route("/comparison/compare-translations",
+   *     name="app_comparison_compare_translations",
+   *     options = { "expose" = true })
+   */
+  public function compareTranslations(Request $request): JsonResponse
+  {
+
+
+    $sourceURL = $request->request->get('source_url');
+    $targetURL = $request->request->get('target_url');
+
+
+    $responseSource = $this->client->request(
+      'GET',
+      $sourceURL
+    );
+
+    $responseTarget = $this->client->request(
+      'GET',
+      $targetURL
+    );
+
+    $source = $responseSource->toArray();
+    $target = $responseTarget->toArray();
+
+
+    $translations = [];
+    foreach ($target as $item => $value) {
+      if (!array_key_exists($item, $source)):
+        $translations[] = [
+          'type' => 'Missing',
+          'key' => $item,
+          'source_value' => '',
+          'target_value' => $value
+        ];
+      else:
+        if ($source[$item] !== $value):
+          $translations[] = [
+            'type' => 'Changed',
+            'key' => $item,
+            'source_value' => $source[$item],
+            'target_value' => $value
+          ];
+        endif;
+      endif;
     }
+
+    foreach ($source as $item => $value) {
+
+      if (!array_key_exists($item, $target)):
+        $translations[] = [
+          'type' => 'Missing',
+          'key' => $item,
+          'source_value' => $value,
+          'target_value' => ''
+        ];
+      endif;
+
+    }
+
+    return new JsonResponse($translations, $responseSource->getStatusCode());
+
+
+  }
+
+  /**
+   * @Route("/comparison/contents", name="app_comparison_contents")
+   */
+  public function contents(): Response
+  {
+    return $this->render('comparison/contents.html.twig', [
+      'controller_name' => 'ComparisonController',
+    ]);
+  }
 }
